e3b393d86106538fb556aed3645913017eb18f71
GuilhermeMDev <guimagioni@gmail.com>
2023-01-26 13:00:41 -0300

    new feat: add new method to return repetitiveGroup

--- app/Http/Controllers/DiveTableController.php
+++ app/Http/Controllers/DiveTableController.php
@@ -12,29 +12,37 @@ class DiveTableController extends Controller
      *
      * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|object
      */
-    public function index(Request $request)
+    public function index(Request $request) //Lista todas as tabelas, e se informar o parametro depth na URL ex: /api/tive-table-letter/?depth=11 retorna só o perfil do mergulho da profundidade consultada.
     {
-        return DataDive::query()
-            ->where('depth', '>=', (int)$request->query->get('depth')) //type cast -> forçando um tipo
-            ->orderBy('depth')
-            ->first();
+        if ((int)$request->query->get('depth')) {
+            return DataDive::query()
+                ->where('maxfsw', '>=', (int)$request->query->get('depth')) //type cast -> forçando um tipo
+                ->orderBy('maxfsw')
+                ->first();
+        }
+
+        return DataDive::all();
     }
 
     public function repetitiveGroup(Request $request)
     {
-        $dataDive = DataDive::query()
-            ->where('depth', '>=', (int)$request->query->get('depth'))
-            ->orderBy('depth')
-            ->first();
+        //Encontrando a tabela com base na profundidade informada
+        $repetitiveLetter = DataDive::query()
+            ->where('maxfsw', '>=', (int)$request->query->get('depth'))
+            ->first()->getAttribute('values');
 
-//        dd($dataDive);
+        //Aqui chega um inteiro, profundidade informada pelo client
+        $depthTime = (int)$request->query->get('depthTime');
 
-        $dataTime = DataDive::query()
-            ->where('A', '>=', (int)$request->query->get('depthTime'))
-            ->first();
+        //Após identificar a tabela através da profundidade, comparo o tempo de fundo para obter o Grupo Repetitivo
+        foreach ($repetitiveLetter as $letter){
 
-//            return [$dataDive, $dataTime];
-        dd($dataDive, $dataTime);
+            if ($depthTime >= $letter['minTime'] && $depthTime <= $letter['maxTime']){
+                return $letter['groupLetter'];
+            };
+
+
+        }
     }
 
 
